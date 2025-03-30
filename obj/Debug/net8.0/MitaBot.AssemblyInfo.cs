1794dae1dcb34156167b858d73b500dc04cc2a3e
Tomython <dmitrymacpro@gmail.com>
2025-03-30 03:58:17 +0300

    Уменьшил запрос

--- obj/Debug/net8.0/MitaBot.AssemblyInfo.cs
+++ obj/Debug/net8.0/MitaBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MitaBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3384c69a2a0314f4e8544104b179452d2580135f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e458d962b77cc821008d3a416d48622e8c74ed4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MitaBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MitaBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
